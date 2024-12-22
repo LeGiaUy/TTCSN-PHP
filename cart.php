0cb06cd79f7ba8cd5612f160ab446daf243a26ed
Le Gia Uy <leuy091104@gmail.com>
2024-12-22 16:49:40 +0700

    Cart Feature

--- cart.php
+++ cart.php
@@ -1,3 +1,116 @@
+<?php
+
+session_start();
+
+if(isset($_POST['add_to_cart'])){
+
+    // Neu trong gio co san pham
+    if(isset($_SESSION['cart'])){
+
+        $products_array_ids = array_column($_SESSION['cart'], "product_id");
+        //Check neu san pham da co trong gio chua
+        if(!in_array($_POST['product_id'], $products_array_ids)){
+
+            $product_id = $_POST['product_id'];
+
+                $product_array = array(
+                                'product_id' => $_POST['product_id'],
+                                'product_name' => $_POST['product_name'],
+                                'product_price' => $_POST['product_price'],
+                                'product_image' => $_POST['product_image'],
+                                'product_quantity' => $_POST['product_quantity'],
+            );
+
+        $_SESSION['cart'][$product_id] = $product_array;
+        }
+        // san pham da co trong gio hang
+        else{
+
+            echo '<script>alert("Sản phẩm đã có trong giỏ hàng");</script>';
+        }
+    }
+    // Neu trong gio chua co san pham
+    else{
+
+        $product_id = $_POST['product_id'];
+        $product_name = $_POST['product_name'];
+        $product_price = $_POST['product_price'];
+        $product_image = $_POST['product_image'];
+        $product_quantity = $_POST['product_quantity'];
+
+        $product_array = array(
+                        'product_id' => $product_id,
+                        'product_name' => $product_name,
+                        'product_price' => $product_price,
+                        'product_image' => $product_image,
+                        'product_quantity' => $product_quantity,
+        );
+
+        $_SESSION['cart'][$product_id] = $product_array;
+        // [ 2=>[], 3=>[] ] mang cang mang san pham voi unique id la product id
+
+    }
+
+    //tinh tong tien
+    calculateTotalCart();
+}
+
+// xoa san pham khoi gio hang
+else if(isset($_POST['remove_product'])) {
+
+    $product_id = $_POST['product_id'];
+    unset($_SESSION['cart'][$product_id]);
+
+    //tinh tong tien
+    calculateTotalCart();
+
+}
+
+else if(isset($_POST['edit_quantity'])) {
+
+    //lay id va so luong tu form
+    $product_id = $_POST['product_id'];
+    $product_quantity = $_POST['product_quantity'];
+
+    //lay mang san pham tu session
+    $product_array =  $_SESSION['cart'][$product_id];
+    
+    //cap nhat so luong san pham trong mang
+    $product_array['product_quantity'] = $product_quantity;
+
+    //tra lai mang ve session
+    $_SESSION['cart'][$product_id] = $product_array;
+
+    //tinh tong tien
+    calculateTotalCart();
+
+}
+
+else{
+    header('location: index.php');
+}
+
+function calculateTotalCart(){
+
+    $total = 0;
+
+    foreach($_SESSION['cart'] as $key => $value){
+
+        $product = $_SESSION['cart'][$key];
+
+        $price =  $product['product_price'];
+        $quantity =  $product['product_quantity'];
+
+        $total = $total + ($price * $quantity);
+
+    }
+
+    $_SESSION['total'] = $total;
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,82 +177,51 @@
                 <th>Số lượng</th>
                 <th>Tổng tiền</th>
             </tr>
+
+            <?php foreach($_SESSION['cart'] as $key => $value){ ?>
+
             <tr>
                 <td>
                     <div class="product-info">
-                        <img src="assets/imgs/featured1.webp"/>
+                        <img src="assets/imgs/<?php echo $value['product_image']; ?>"/>
                         <div>
-                            <p>Iphone 16 Pro Max</p>
-                            <small><span>$</span>999</small>
+                            <p><?php echo $value['product_name']; ?></p>
+                            <small><span>$</span><?php echo $value['product_price']; ?></small>
                             <br>
-                            <a class="remove-btn" href="#">Xóa</a>
+                            <form method="POST" action="cart.php">
+                                <input type="hidden" name="product_id" value="<?php echo $value['product_id']; ?>"/>
+                                <input type="submit" name="remove_product" class="remove-btn" value="Xóa"></input>
+                            </form>
+                            
                         </div>
                     </div>
                 </td>
 
                 <td>
-                    <input type="number" value="1"/>
-                    <a class="edit-btn" href="#">Chỉnh sửa</a>
+                    
+                    <form method="POST" action="cart.php">
+                        <input type="hidden" name="product_id" value="<?php echo $value['product_id']; ?>"/>
+                        <input type="number" name="product_quantity" value="<?php echo $value['product_quantity']; ?>"/>
+                        <input type="submit" class="edit-btn" value="Sửa" name="edit_quantity"/>
+                    </form>
+                    
                 </td>
 
                 <td>
                     <span>$</span>
-                    <span class="product-price">999</span>
+                    <span class="product-price"><?php echo $value['product_quantity'] * $value['product_price'] ?></span>
                 </td>
             </tr>
-            <tr>
-                <td>
-                    <div class="product-info">
-                        <img src="assets/imgs/featured1.webp"/>
-                        <div>
-                            <p>Iphone 16 Pro Max</p>
-                            <small><span>$</span>999</small>
-                            <br>
-                            <a class="remove-btn" href="#">Xóa</a>
-                        </div>
-                    </div>
-                </td>
 
-                <td>
-                    <input type="number" value="1"/>
-                    <a class="edit-btn" href="#">Chỉnh sửa</a>
-                </td>
-
-                <td>
-                    <span>$</span>
-                    <span class="product-price">999</span>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="product-info">
-                        <img src="assets/imgs/featured1.webp"/>
-                        <div>
-                            <p>Iphone 16 Pro Max</p>
-                            <small><span>$</span>999</small>
-                            <br>
-                            <a class="remove-btn" href="#">Xóa</a>
-                        </div>
-                    </div>
-                </td>
-
-                <td>
-                    <input type="number" value="1"/>
-                    <a class="edit-btn" href="#">Chỉnh sửa</a>
-                </td>
-
-                <td>
-                    <span>$</span>
-                    <span class="product-price">999</span>
-                </td>
-            </tr>
+            <?php }?>
+            
         </table>
 
         <div class="cart-total">
             <table>
                 <tr>
                     <td>Tổng tiền</td>
-                    <td>$999</td>
+                    <td>$ <?php echo $_SESSION['total'];?></td>
                 </tr>
             </table>
         </div>
--- single_product.php
+++ single_product.php
@@ -85,35 +85,48 @@ if (isset($_GET['product_id'])) {
     <section class="container single-product my-5 pt-5">
         <div class="row mt-5">
             <?php while($row = $product->fetch_assoc()){ ?>
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <img class="img-fluid w-100 pb-1" src="assets/imgs/<?php echo $row['product_image']; ?>" id="mainImg"/>
-                <div class="small-img-group">
-                    <div class="small-img-col">
-                        <img src="assets/imgs/<?php echo $row['product_image']; ?>" width="100%" class="small-img"/>
-                    </div>
-                    <div class="small-img-col">
-                        <img src="assets/imgs/<?php echo $row['product_image2']; ?>" width="100%" class="small-img"/>
-                    </div>
-                    <div class="small-img-col">
-                        <img src="assets/imgs/<?php echo $row['product_image3']; ?>" width="100%" class="small-img"/>
-                    </div>
-                    <div class="small-img-col">
-                        <img src="assets/imgs/<?php echo $row['product_image4']; ?>" width="100%" class="small-img"/>
+
+            
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <img class="img-fluid w-100 pb-1" src="assets/imgs/<?php echo $row['product_image']; ?>" id="mainImg"/>
+                    <div class="small-img-group">
+                        <div class="small-img-col">
+                            <img src="assets/imgs/<?php echo $row['product_image']; ?>" width="100%" class="small-img"/>
+                        </div>
+                        <div class="small-img-col">
+                            <img src="assets/imgs/<?php echo $row['product_image2']; ?>" width="100%" class="small-img"/>
+                        </div>
+                        <div class="small-img-col">
+                            <img src="assets/imgs/<?php echo $row['product_image3']; ?>" width="100%" class="small-img"/>
+                        </div>
+                        <div class="small-img-col">
+                            <img src="assets/imgs/<?php echo $row['product_image4']; ?>" width="100%" class="small-img"/>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-lg-6 col-md-12 col-sm-12">
-                <h6>Apple</h6>
-                <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
-                <h2>$<?php echo $row['product_price']; ?></h2>
-                <input type="number" value="1"/>
-                <button class="buy-btn">Thêm Vào Giỏ Hàng</button>
-                <h4 class="mt-5 mb-5">Mô Tả</h4>
-                <span>
-                    <?php echo $row['product_description']; ?>
-                </span>
-            </div>
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <h6>Apple</h6>
+                    <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
+                    <h2>$<?php echo $row['product_price']; ?></h2>
+
+                    <!-- Form thêm sản phẩm vào giỏ hàng -->
+                    <form method="POST" action="cart.php">
+                        <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>"/>
+                        <input type="hidden" name="product_image" value="<?php echo $row['product_image']; ?>"/>
+                        <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>"/>
+                        <input type="hidden" name="product_price" value="<?php echo $row['product_price']; ?>"/>
+
+                            <input type="number" name="product_quantity" value="1"/>
+                            <button class="buy-btn" type="submit" name="add_to_cart">Thêm Vào Giỏ Hàng</button>
+                    
+                    </form>        
+                    
+                    <h4 class="mt-5 mb-5">Mô Tả</h4>
+                    <span>
+                        <?php echo $row['product_description']; ?>
+                    </span>
+                </div>          
             <?php } ?>
         </div>
     </section>
