8da624901ea012db194329d69ebbd064cd099a89
Le Gia Uy <leuy091104@gmail.com>
2024-12-23 09:01:00 +0700

    Order Status

--- order_details.php
+++ order_details.php
@@ -5,6 +5,7 @@ include('server/connection.php');
 if(isset($_POST['order_details_btn']) && isset($_POST['order_id'])){
 
     $order_id = $_POST['order_id'];
+    $order_status = $_POST['order_status'];
 
     $stmt = $conn->prepare("SELECT * FROM order_items WHERE order_id = ?");
 
@@ -114,6 +115,14 @@ else{
 
         <?php }?>
     </table>
+
+    <?php
+
+    if($order_status == "Chưa thanh toán"){ ?>
+        <form style="float: right;">
+            <input type="submit" class="btn btn-primary" value="Thanh toán"></input>
+        </form>
+    <?php }?>
     </section>
 
     <!-- Footer -->
